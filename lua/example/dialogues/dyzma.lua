cc31df6bf4cbda4937e35f461b3272a4f8412c96
Sventimir <svantimir@gmail.com>
2025-09-22 15:19:09 +0200

    Fix bugs in Dyzma's dialogue

--- lua/example/dialogues/dyzma.lua
+++ lua/example/dialogues/dyzma.lua
@@ -13,8 +13,10 @@ function Dyzma:new(spec)
     d:add(Dialogue.Line:new(snd, "Bo taki jest Las, ile Dyzmy wśród nas."))
   end
   d:add(Dialogue.Line:new(dyzma, "Najważniejsze dla mnie abym żył jak drzewo."))
-  d:add(Dialogue.Line:new(fst, "Za długo już nie pożyjesz, Bratku."))
+  d:add(Dialogue.Line:new(fst, "Za długo już nie pożyjesz, bratku."))
   d:add(Dialogue.Line:new(dyzma, "No tak to już dawno mnie nie obrażono. Czy ja wyglądam jak kwiat? Zginiecie za to!"))
+
+  return d
 end
 
 return Dyzma
