bf10002d4884695ff6e33eb225dd114835e9ac6e
Sventimir <svantimir@gmail.com>
2025-09-25 22:22:50 +0200

    Improve Dyzma's summoning dialogue

--- lua/example/dialogues/dyzma.lua
+++ lua/example/dialogues/dyzma.lua
@@ -6,15 +6,25 @@ function Dyzma:new(spec)
   local dyzma = wesnoth.units.find({ id = "forest-boss"})[1]
   local fst = wesnoth.units.find({ id = wml.variables.summoner.id })[1]
   local snd = wesnoth.units.find({ side = spec.player_sides, wml.tag["not"]({ id = fst.id })})[1]
+  local side_numbers = string.split(spec.player_sides, ",")
+  local panzer = wesnoth.units.create({
+      type = "Heavy Infantryman",
+      side = side_numbers[mathx.random(#player_sides)],
+  })
   local d = Dialogue:new()
 
   d:add(Dialogue.Line:new(fst, "Czy Las się przyzna, że w nim jest Dyzma?"))
   if snd then
     d:add(Dialogue.Line:new(snd, "Bo taki jest Las, ile Dyzmy wśród nas."))
   end
-  d:add(Dialogue.Line:new(dyzma, "Najważniejsze dla mnie abym żył jak drzewo."))
-  d:add(Dialogue.Line:new(fst, "Za długo już nie pożyjesz, bratku."))
-  d:add(Dialogue.Line:new(dyzma, "No tak to już dawno mnie nie obrażono. Czy ja wyglądam jak kwiat? Zginiecie za to!"))
+  d:add(Dialogue.Line:new(dyzma, "Ludzie to za dużo teraz żrą!"))
+  d:add(Dialogue.Line:new(dyzma, "Ludzie coraz cięższe teraz są!"))
+  d:add(Dialogue.Line:new(panzer, "Uważaj sobie! Gdybym nie był związany, to tak bym ci przywalił moją pałą...")) 
+  d:add(Dialogue.Line:new(dyzma, "... ale najważniejsze dla mnie abym żył jak drzewo."))
+  d:add(Dialogue.Line:new(panzer, "To drzewo jest zdrowo jebnięte. I to nie moja sprawka, niestety."))
+  d:add(Dialogue.Line:new(panzer, "Zetnijcie je co rychlej i uwolnijcie mnie, a przyłączę się do was."))
+  d:add(Dialogue.Line:new(fst, "Zobaczymy, co da się zrobić."))
+  wml.variables.panzer_captive = panzer
 
   return d
 end
